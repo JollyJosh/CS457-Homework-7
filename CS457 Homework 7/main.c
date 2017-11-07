f5645f84e5876ddd183793bcc206e45e97f1da51
Josh Collins <jjcollins2@crimson.ua,edu>
2017-11-07 16:55:03 -0600

    Initial Commit

---
+++ CS457 Homework 7/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS457 Homework 7
+//
+//  Created by Josh Collins on 11/7/17.
+//  Copyright © 2017 Josh Collins. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
